e54bf09fe1dcc5582439f2c58f1735e208d15e30
AslanHasanov <aslanrh@code.edu.az>
2022-07-14 19:32:07 +0400

    Initial Commit

---
+++ Person Managment/Person Managment/UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Person_Managment.UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
