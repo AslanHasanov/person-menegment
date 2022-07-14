ec7c0ff1721137ff36d255b3a4e6855f6a729eeb
elnurmehdi <elnurkm@code.edu.az>
2022-07-14 20:56:37 +0400

    Update method added EmployeeRepository class

--- Person Managment/Person Managment/Database/Repository/EmployeeRepository.cs
+++ Person Managment/Person Managment/Database/Repository/EmployeeRepository.cs
@@ -9,16 +9,35 @@ namespace Person_Managment.Database.Repository
 {
     class EmployeeRepository
     {
-        public static List<Employee> _person = new List<Employee>();
+        public static List<Employee> _persons = new List<Employee>();
         
         public static Employee Add(string name, string surname, string fatherName, string fin, string email)
         {
             Employee person = new Employee(name, surname, fatherName, fin, email);
 
 
-            _person.Add(person);
+            _persons.Add(person);
             return person;
             
+        }
+
+        public static Employee Update(string name, int id ,string surname, string fatherName, string fin, string email)
+        {
+            foreach (Employee person in _persons)
+            {
+                if (person.Id == id)
+                {
+                    person.Name = name;
+                    person.Surname = surname;
+                    person.FatherName = fatherName;
+                    person.FIN = fin;
+                    person.Email = email;
+
+                    return person;
+
+                }
+            }
+            return null;
         }
 
     }
