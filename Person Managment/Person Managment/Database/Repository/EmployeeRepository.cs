0c862e02b35890ffd1d7bc5fe7f3d1de36dfa0f4
elnurmehdi <elnurkm@code.edu.az>
2022-07-14 20:42:45 +0400

    <Add> method added to <EmployeeRepository> class

--- Person Managment/Person Managment/Database/Repository/EmployeeRepository.cs
+++ Person Managment/Person Managment/Database/Repository/EmployeeRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Person_Managment.Database.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,17 @@ namespace Person_Managment.Database.Repository
 {
     class EmployeeRepository
     {
+        public static List<Employee> _person = new List<Employee>();
+        
+        public static Employee Add(string name, string surname, string fatherName, string fin, string email)
+        {
+            Employee person = new Employee(name, surname, fatherName, fin, email);
+
+
+            _person.Add(person);
+            return person;
+            
+        }
+
     }
 }
