3764f1299272300804b01e6bb0cb892cab6b0a54
AslanHasanov <aslanrh@code.edu.az>
2022-07-14 20:19:46 +0400

    Adress class added to models folder(modified)

--- Person Managment/Person Managment/Database/Models/Adress.cs
+++ Person Managment/Person Managment/Database/Models/Adress.cs
@@ -8,16 +8,16 @@ namespace Person_Managment.DataBase.Models
 {
     internal class Adress
     {
-        public int Id { get;private set; }
+        public int Id { get; private set; }
         public string Name { get; set; }
 
         public int _counter = 1;
 
-        public Adress( string name)
+        public Adress(string name)
         {
             Id = _counter++;
             Name = name;
-            
+
         }
     }
 }
