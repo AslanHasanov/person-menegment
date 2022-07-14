0db887b0338729fbed8bb9948d45139883c1a737
AslanHasanov <aslanrh@code.edu.az>
2022-07-14 20:44:01 +0400

    Add method added to AdressRepository class

---
+++ Person Managment/Person Managment/Database/Repository/AdressRepository.cs
@@ -0,0 +1,21 @@
+﻿using Person_Managment.DataBase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person_Managment.DataBase.Repository
+{
+    internal class AdressRepository
+    {
+       public static List<Adress> _adresses = new List<Adress>();
+
+        public static Adress Add(string name)
+        {
+            Adress adress = new Adress(name);
+            _adresses.Add(adress);
+            return adress;
+        }
+    }
+}
