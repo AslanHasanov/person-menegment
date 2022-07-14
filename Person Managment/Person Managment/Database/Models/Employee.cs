328414341ba51118eccda42fa6751c1108ebdc93
elnurmehdi <elnurkm@code.edu.az>
2022-07-14 19:55:41 +0400

    Modified Employee class added to source codes

--- Person Managment/Person Managment/Database/Models/Employee.cs
+++ Person Managment/Person Managment/Database/Models/Employee.cs
@@ -8,5 +8,31 @@ namespace Person_Managment.Database.Models
 {
     class Employee
     {
+        public int Id { get; private set; }
+
+
+        public string Name { get; set; }
+
+        public string  Surname { get; set; }
+
+        public string  FatherName { get; set; }
+
+        public string  FIN { get; set; }
+
+        public string  Email { get; set; }
+
+        private static int _idCounter = 1;
+
+
+        public Employee(string name, string surname, string fatherName, string fin, string email)
+        {
+            Id = _idCounter; 
+            Name = name;
+            Surname = surname;
+            FatherName = fatherName;
+            FIN = fin;
+            Email = email;
+            _idCounter++;
+        }
     }
 }
